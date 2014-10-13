0ec6d1cfb883e4c63b768b8fc4a66203a5f24c81
David Gomez <dgomezperez@gmail.com>
2014-10-13 12:31:00 +0200

    improve Test add DataSet

--- connector-deep-core/src/test/java/com/stratio/connector/deep/CommonsPrepareTestData.java
+++ connector-deep-core/src/test/java/com/stratio/connector/deep/CommonsPrepareTestData.java
@@ -1,16 +1,18 @@
 package com.stratio.connector.deep;
 
-public interface CommonsPrepareTestQuerys {
+public interface CommonsPrepareTestData {
 
     public static final String DROP_KEYSPACE =" DROP KEYSPACE IF EXISTS %s  ";
 
     public static final String CREATE_KEYSPACE =" CREATE  KEYSPACE IF NOT EXISTS %s WITH replication = { 'class' : " +
             "'SimpleStrategy', 'replication_factor' : 1 } ";
 
-    public static final String rawInsert = "INSERT INTO %s (" + "\"id\", \"artist\", \"title\", "
+    public static final String rawSongsInsert = "INSERT INTO %s (" + "\"id\", \"artist\", \"title\", "
             + "\"year\", \"length\", " + "\"description\") "
             + "values (%s, \'%s\',\'%s\', \'%s\', \'%s\', \'%s\');";
 
+    public static final String rawArtistsInsert = "INSERT INTO %s (" + "\"id\", \"artist\", \"age\") "
+            + "values (%s, \'%s\',\'%s\');";
     public static final String CREATE_TABLE =" DROP KEYSPACE ";
 
 
