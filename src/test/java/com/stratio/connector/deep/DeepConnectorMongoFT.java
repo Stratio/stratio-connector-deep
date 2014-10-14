e9fa0c9372f44f7a5238c73fe0a35f4ab83b4592
Óscar Puertas <opuertas@stratio.com>
2014-10-14 10:36:05 +0200

    Arranging connector folders

--- src/test/java/com/stratio/connector/deep/DeepConnectorMongoFT.java
+++ src/test/java/com/stratio/connector/deep/DeepConnectorMongoFT.java
@@ -113,43 +113,84 @@ public class DeepConnectorMongoFT {
         Project project = createProject(MONGO_CLUSTERNAME_CONSTANT, KEYSPACE, MYTABLE1_CONSTANT,
                 Arrays.asList(AUTHOR_CONSTANT, DESCRIPTION_CONSTANT, TITLE_CONSTANT, YEAR_CONSTANT));
 
-        //for (Operator op : Operator.values()){
+        for (Operator op : Operator.values()){
 
-        project.setNextStep(createFilter(KEYSPACE, MYTABLE1_CONSTANT, YEAR_CONSTANT , Operator.DISTINCT, YEAR_EX ));
+            if(op.isInGroup(Operator.Group.COMPARATOR) && !op.equals(Operator.IN) && !op.equals(Operator.BETWEEN)){
+                project.setNextStep(createFilter(KEYSPACE, MYTABLE1_CONSTANT, YEAR_CONSTANT , op , YEAR_EX ));
 
-        //}
 
-        LogicalStep filter =  project.getNextStep();
+                LogicalStep filter =  project.getNextStep();
 
-        filter.setNextStep(createSelect(Arrays.asList(createColumn(KEYSPACE, MYTABLE1_CONSTANT,
-                AUTHOR_CONSTANT)), Arrays.asList(AUTHOR_ALIAS_CONSTANT)));
+                filter.setNextStep(createSelect(Arrays.asList(createColumn(KEYSPACE, MYTABLE1_CONSTANT,
+                        AUTHOR_CONSTANT)), Arrays.asList(AUTHOR_ALIAS_CONSTANT)));
 
-        // One single initial step
-        stepList.add(project);
+                // One single initial step
+                stepList.add(project);
 
-        LogicalWorkflow logicalWorkflow = new LogicalWorkflow(stepList);
+                LogicalWorkflow logicalWorkflow = new LogicalWorkflow(stepList);
 
-        // Execution
-        QueryResult result = deepQueryEngine.executeWorkFlow(logicalWorkflow);
+                // Execution
+                QueryResult result = deepQueryEngine.executeWorkFlow(logicalWorkflow);
 
-        // Assertions
-        List<ColumnMetadata> columnsMetadata = result.getResultSet().getColumnMetadata();
-        List<Row> rowsList = result.getResultSet().getRows();
+                // Assertions
+                List<ColumnMetadata> columnsMetadata = result.getResultSet().getColumnMetadata();
+                List<Row> rowsList = result.getResultSet().getRows();
 
-        // Checking results number
-        assertEquals("Wrong number of rows metadata", 1, columnsMetadata.size());
-        assertEquals("Wrong number of rows", 1, rowsList.size());
+                // Checking results number
+                int resultExpectedFomOp = getResultExpectedFomOp(op);
+                assertEquals("Wrong number of rows metadata", 1, columnsMetadata.size());
+                assertEquals("Wrong number of rows", resultExpectedFomOp, rowsList.size());
 
-        // Checking metadata
-        assertEquals("Author expected", AUTHOR_CONSTANT, columnsMetadata.get(0).getColumnName());
-        assertEquals("mytable1 expected", KEYSPACE + "." + MYTABLE1_CONSTANT, columnsMetadata.get(0)
-                .getTableName());
+                // Checking metadata
+                assertEquals("Author expected", AUTHOR_CONSTANT, columnsMetadata.get(0).getColumnName());
+                assertEquals("mytable1 expected", KEYSPACE + "." + MYTABLE1_CONSTANT, columnsMetadata.get(0)
+                        .getTableName());
 
-        // Checking rows
-        for (Row row : rowsList) {
-            assertEquals("Wrong number of columns in the row", 1, row.size());
-            assertNotNull("Expecting author column in row", row.getCell(AUTHOR_ALIAS_CONSTANT));
+                // Checking rows
+                for (Row row : rowsList) {
+                    assertEquals("Wrong number of columns in the row", 1, row.size());
+                    assertNotNull("Expecting author column in row", row.getCell(AUTHOR_ALIAS_CONSTANT));
+                }
+            }
+
         }
+
+
+    }
+
+    private int getResultExpectedFomOp(Operator op) {
+
+        int result =0;
+
+        switch (op){
+
+            case EQ:
+                result=2;
+                break;
+            case LT:
+                result= 183;
+                break;
+            case GT:
+                result= 25;
+                break;
+            case LET:
+                result= 185;
+                break;
+            case GET:
+                result= 27;
+                break;
+            case DISTINCT:
+                result= 208;
+                break;
+            default:
+                result=210;
+                break;
+
+
+        }
+
+
+        return result;
     }
 
 }
