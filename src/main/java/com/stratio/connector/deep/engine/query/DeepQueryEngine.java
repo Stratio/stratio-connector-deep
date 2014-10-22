eee29f2a863dd92b4713b7e6cab275530480276d
Óscar Puertas <opuertas@stratio.com>
2014-10-22 11:46:35 +0200

    Fixing license warning in Sonar

--- src/main/java/com/stratio/connector/deep/engine/query/DeepQueryEngine.java
+++ src/main/java/com/stratio/connector/deep/engine/query/DeepQueryEngine.java
@@ -4,38 +4,31 @@
  * (C) licenses this file to you under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of the License at
  * 
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package com.stratio.connector.deep.engine.query;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.spark.api.java.JavaRDD;
 
 import com.stratio.connector.commons.engine.CommonsQueryEngine;
 import com.stratio.connector.deep.connection.DeepConnectionHandler;
-import com.stratio.deep.commons.entity.Cells;
-import com.stratio.deep.core.context.DeepSparkContext;
 import com.stratio.crossdata.common.connector.IResultHandler;
+import com.stratio.crossdata.common.data.ClusterName;
 import com.stratio.crossdata.common.exceptions.ExecutionException;
 import com.stratio.crossdata.common.exceptions.UnsupportedException;
 import com.stratio.crossdata.common.logicalplan.LogicalWorkflow;
 import com.stratio.crossdata.common.result.QueryResult;
-import com.stratio.crossdata.common.data.ClusterName;
+import com.stratio.deep.core.context.DeepSparkContext;
 
 public class DeepQueryEngine extends CommonsQueryEngine {
 
     private final DeepSparkContext deepContext;
 
     private final DeepConnectionHandler deepConnectionHandler;
-
-
 
     public DeepQueryEngine(DeepSparkContext deepContext, DeepConnectionHandler deepConnectionHandler) {
         super(deepConnectionHandler);
@@ -46,7 +39,7 @@ public class DeepQueryEngine extends CommonsQueryEngine {
     @Override
     @Deprecated
     public QueryResult execute(ClusterName targetCluster, LogicalWorkflow workflow) throws UnsupportedException,
-                    ExecutionException {
+            ExecutionException {
 
         return execute(workflow);
     }
@@ -54,7 +47,9 @@ public class DeepQueryEngine extends CommonsQueryEngine {
     /*
      * (non-Javadoc)
      * 
-     * @see com.stratio.crossdata.common.connector.IQueryEngine#execute(com.stratio.crossdata.common.logicalplan.LogicalWorkflow)
+     * @see
+     * com.stratio.crossdata.common.connector.IQueryEngine#execute(com.stratio.crossdata.common.logicalplan.LogicalWorkflow
+     * )
      */
     @Override
     public QueryResult executeWorkFlow(LogicalWorkflow workflow) throws UnsupportedException, ExecutionException {
@@ -71,7 +66,7 @@ public class DeepQueryEngine extends CommonsQueryEngine {
      */
     @Override
     public void asyncExecute(String queryId, LogicalWorkflow workflow, IResultHandler resultHandler)
-                    throws UnsupportedException, ExecutionException {
+            throws UnsupportedException, ExecutionException {
         // TODO Auto-generated method stub
 
     }
