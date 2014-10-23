25d14fa8b210688d1b952fe758a5d7efdc9c3272
Óscar Puertas <opuertas@stratio.com>
2014-10-23 09:44:26 +0200

    Changes suggested by Sonar

--- src/main/java/com/stratio/connector/deep/engine/query/structures/Term.java
+++ src/main/java/com/stratio/connector/deep/engine/query/structures/Term.java
@@ -124,14 +124,14 @@ public abstract class Term<T extends Comparable<T>> extends ValueCell<T> impleme
         }
         if (!(this.clazz.isInstance(obj))) {
             try {
-                if (obj.getClass().equals(String.class)) {
+                if (obj instanceof String) {
                     return this.getStringValue().equals(obj);
-                } else if (obj.getClass().equals(Float.class)) {
-                    return Float.valueOf(this.getStringValue()).equals((Float) obj);
-                } else if (obj.getClass().equals(Integer.class)) {
-                    return Integer.valueOf(this.getStringValue()).equals((Integer) obj);
-                } else if (obj.getClass().equals(Long.class)) {
-                    return Long.valueOf(this.getStringValue()).equals((Long) obj);
+                } else if (obj instanceof Float) {
+                    return Float.valueOf(this.getStringValue()).equals(obj);
+                } else if (obj instanceof Integer) {
+                    return Integer.valueOf(this.getStringValue()).equals(obj);
+                } else if (obj instanceof Long) {
+                    return Long.valueOf(this.getStringValue()).equals(obj);
                 }
             } catch (NumberFormatException e) {
                 LOG.error("Sorry, unable to Cast incompatible types ->" + this.clazz + " & " + obj.getClass());
